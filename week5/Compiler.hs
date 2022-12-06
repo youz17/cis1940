f6cc59208e3f71059acd80c18c002954b1d7c819
youz <youz17@qq.com>
2022-12-06 23:12:05 +0800

    add week5 exercise5 todo

--- week5/Compiler.hs
+++ week5/Compiler.hs
@@ -3,5 +3,9 @@
 
 import StackVM (Program)
 
+-- todo: 等学完 Monad, Functor 之类再来看看
 compile :: String -> Maybe Program
-compile es = undefined
+compile = undefined
+
+-- compile "1+2*3"
+-- Push 2, Push 3, Mul, Push 1, Add
