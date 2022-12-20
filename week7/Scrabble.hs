a82d5af6883b4bf0680fa4151a8af55394d84e44
youz <youz17@qq.com>
2022-12-21 00:18:00 +0800

    week7 exercise 3

--- week7/Scrabble.hs
+++ week7/Scrabble.hs
@@ -1,1 +1,57 @@
-module Scrabble where
+{-# OPTIONS_GHC -Wall #-}
+
+module Scrabble (Score, score, scoreString, scoreLine) where
+
+import Data.Char (toUpper)
+import Data.Set qualified as S
+import JoinList (JoinList (Single))
+
+newtype Score = Score Int
+  deriving (Eq, Ord, Show, Num)
+
+instance Semigroup Score where
+  (<>) = (+)
+
+instance Monoid Score where
+  mempty = 0
+
+createScoreSet :: [Char] -> S.Set Char
+createScoreSet cl = S.fromList (cl ++ map toUpper cl)
+
+oneScoreSet :: S.Set Char
+oneScoreSet = createScoreSet "aeilnorstu"
+
+twoScoreSet :: S.Set Char
+twoScoreSet = createScoreSet "dg"
+
+threeScoreSet :: S.Set Char
+threeScoreSet = createScoreSet "bcmp"
+
+fourScoreSet :: S.Set Char
+fourScoreSet = createScoreSet "fhvwy"
+
+fiveScoreSet :: S.Set Char
+fiveScoreSet = createScoreSet "k"
+
+eightScoreSet :: S.Set Char
+eightScoreSet = createScoreSet "jx"
+
+tenScoreSet :: S.Set Char
+tenScoreSet = createScoreSet "qz"
+
+score :: Char -> Score
+score c
+  | S.member c oneScoreSet = 1
+  | S.member c twoScoreSet = 2
+  | S.member c threeScoreSet = 3
+  | S.member c fourScoreSet = 4
+  | S.member c fiveScoreSet = 5
+  | S.member c eightScoreSet = 8
+  | S.member c tenScoreSet = 10
+  | otherwise = 0
+
+scoreString :: String -> Score
+scoreString = sum . map score
+
+scoreLine :: String -> JoinList Score String
+scoreLine s = Single (scoreString s) s
