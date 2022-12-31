d593949767384339eb02f6488a45515734bd336d
youz <youz17@qq.com>
2022-12-31 18:00:40 +0800

    week7 test

--- week7/StringBufEditor.hs
+++ week7/StringBufEditor.hs
@@ -1,11 +1,17 @@
 module Main where
 
-import StringBuffer
+import Buffer
 import Editor
+import JoinList (JoinListBuffer)
 
-main = runEditor editor $ unlines
-         [ "This buffer is for notes you don't want to save, and for"
-         , "evaluation of steam valve coefficients."
-         , "To load a different file, type the character L followed"
-         , "by the name of the file."
-         ]
+jlFromLines :: [String] -> JoinListBuffer
+jlFromLines = fromString . unlines
+
+main =
+  runEditor editor $
+    jlFromLines
+      [ "This buffer is for notes you don't want to save, and for",
+        "evaluation of steam valve coefficients.",
+        "To load a different file, type the character L followed",
+        "by the name of the file."
+      ]
